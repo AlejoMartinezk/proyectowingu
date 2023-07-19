f8ad159549f0396f231bb2801c27664c752e5fd8
AlejoMartinezk <111890363+AlejoMartinezk@users.noreply.github.com>
2023-07-19 11:18:53 -0300

    Add files via upload

---
+++ ProyectoWingu/script.js
@@ -0,0 +1,10 @@
+function redirigirIg() {
+    window.location.href = "https://www.instagram.com/winguweb/";
+  }
+  function redirigirFace() {
+    window.location.href = "https://www.facebook.com/WinguOSC";
+  }
+  function redirigirLn() {
+    window.location.href = "https://www.linkedin.com/company/1760740";
+  }
+  
